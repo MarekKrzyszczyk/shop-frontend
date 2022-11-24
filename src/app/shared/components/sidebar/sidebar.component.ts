08ef0714e6e125917d83123bb7ea5f31a5d1e015
mkrzyszczyk <mkrzyszczyk@pgs-soft.com>
2022-11-24 19:26:01 +0100

    added angular material and layout style

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'];
 
 }
